c3c3ef23f86902859925de266f6f2f081001356c
Banupriya S <102135250+banupriya2022@users.noreply.github.com>
2024-06-28 20:31:25 +0530

    Create MerchantNotFoundException.java

---
+++ src/main/java/com/alti/baseTemplate/exception/MerchantNotFoundException.java
@@ -0,0 +1,10 @@
+package com.alti.baseTemplate.exception;
+
+public class MerchantNotFoundException extends RuntimeException{
+	public MerchantNotFoundException(String message) {
+		// TODO Auto-generated constructor stub
+		super(message);
+	}
+	
+
+}
