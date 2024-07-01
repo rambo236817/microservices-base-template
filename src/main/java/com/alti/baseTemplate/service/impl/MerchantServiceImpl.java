f70efaa5144ac763d805a9647b3b886f391d68b0
Banupriya <sairammukilan@gmail.com>
2024-07-01 17:54:04 +0530

    Added logs to merchant service

--- src/main/java/com/alti/baseTemplate/service/impl/MerchantServiceImpl.java
+++ src/main/java/com/alti/baseTemplate/service/impl/MerchantServiceImpl.java
@@ -9,10 +9,12 @@ import com.alti.baseTemplate.exception.MerchantNotFoundException;
 import com.alti.baseTemplate.mapper.MerchantMapper;
 import com.alti.baseTemplate.service.repo.MerchantRepository;
 
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
+@Slf4j
 public class MerchantServiceImpl {
 
 	@Autowired
@@ -21,20 +23,21 @@ public class MerchantServiceImpl {
 	private static MerchantMapper mapper = MerchantMapper.INSTANCE;
 
 	public Flux<Merchant> getAllMerchants() {
+		log.info("MerchantService findAll() method is called");
 		return repository.findAll().map(mapper::toDto);
 	}
 
 	public Mono<Merchant> saveMerchant(Merchant dto) throws RuntimeException {
 
+		log.info("Creating new merchants..");
 		MerchantStore merchantStore = mapper.toEntity(dto);
 
 		return repository.save(merchantStore).map(mapper::toDto);
 
 	}
 
-	//testing
 	public Mono<Void> deleteMerchantById(int id) throws MerchantNotFoundException {
-
+		log.info("Check and delete merchants specified by id..");
 		return repository.existsById(id).flatMap(exists -> {
 			if (exists) {
 				return repository.deleteById(id);
@@ -46,7 +49,7 @@ public class MerchantServiceImpl {
 	}
 
 	public Mono<Merchant> getMerchantById(int id) throws MerchantNotFoundException {
-		// Mono<MerchantStore> found = repository.findById(id);
+		log.info("Finding merchants by id...");
 
 		return repository.findById(id).map(mapper::toDto)
 				.switchIfEmpty(Mono.error(new MerchantNotFoundException("Merchant Not Found for id " + id)));
